4b0a6cc4c083fb5c94e2af2dfa44a44cc75f444a
Maaike Izeboud <maaike.izeboud@live.nl>
2024-01-26 10:20:30 +0100

    remove unused functions

--- scripts/myFunctions.py
+++ scripts/myFunctions.py
@@ -1,13 +1,9 @@
-import rioxarray as rioxr
 import xarray as xr
-import glob
 import numpy as np
 import os
 import geopandas as gpd 
 import rasterio as rio
-import warnings
 
-# python file with useful functions to import
 
 def get_tilelist_region(region_polys, sector_ID, gridTiles=None):
     ''' This function selects a predefined sector by name ID from the sector-shapefile 'sector_polys'.
@@ -31,10 +27,6 @@ def get_tilelist_region(region_polys, sector_ID, gridTiles=None):
     tileNums_select = [int(tileNum) for tileNum in tileNums_select]
     return tileNums_select
 
-
-def clip_da_to_iceshelf(data_da, iceshelf_polygon_gpd,drop=False):
-     ''' Clip data (xarray.DataArray) to ice shelf polygon file (geopandas.DataFrame) '''
-     return data_da.rio.clip( iceshelf_polygon_gpd.geometry, iceshelf_polygon_gpd.crs, drop=drop, invert=False)
 
 
 
@@ -118,77 +110,8 @@ def calc_nominal_strain(vx, vy, length_scale_px=1 ,version2 = None , dx=None ):
 
 
 
-def aggregate_region_ds_iceshelf( region_ds, varname , iceshelf_polygon_df, drop_clipped=False ):
-    ''' 
-    Function aggregates the spatial data within the specified (iceshelf) polygons to a single 1D np.array.
-    Any spatial information is lost. NaN values are kept, to be able to combine multiple variables within the same region at a later stage.
-    
-    The output is of shape (Npixels , t) where t is the number of years available in the data.
-
-    To do: implement reading of annual iceshelf polygons (currently working with a single shapefile for all years)
-
-    Input
-    -----
-    region_ds           : xarray.Dataset of (y,x,time). 
-    varname             : name of variable in dataset
-    iceshelf_polygon_df : geopandas.DataFrame of polygon to clip data to
-    drop_clipped        : If True:  Remove all pixels outside of polygon from array
-                          If False: Keep all clipped pixels as NoData in array
-
-    Output
-    ------
-    data_iceshelf_1d_yr     : numpy array of shape (Nsamples,y). Where y is the number of years in the 'time' dimension of region_ds 
-    '''
-
-    # set up empty list
-    values_yearList = []
-
-    # get time dimension of dataset
-    years = region_ds['time'].values # array of years in dataSet
-
-    # Aggregate annual data
-    for year in years: 
-
-        # -- get annual data
-        current_yr_da  = region_ds.sel(time=year)[varname] # specifying variable translates xr.dataSet to xr.dataArray
-        
-        # -- clip to iceshelf
-        current_yr_iceshelf  = clip_da_to_iceshelf(current_yr_da,  iceshelf_polygon_df, drop=drop_clipped) 
-        
-        # -- aggregate values to 1D np.array
-        values_yr  = current_yr_iceshelf.values.reshape( -1,1) # extract values (matrix to array)
-
-        # -- store in list
-        values_yearList.append(values_yr)  # list with yearly np.array
-    
-    # merge list of annual values (1D array) to a np.array 
-    if len(values_yearList) > 1:
-        data_iceshelf_1D_yrs = np.concatenate( values_yearList , axis=1) # shape (Npx, Nyrs)
-    else:
-        data_iceshelf_1D_yrs = values_yearList[0] # np.expand_dims(values_yearList[0],1) # shape (Npx,1)
-
-    return data_iceshelf_1D_yrs
 
 
-
-def fill_nan_cdata(xdata,ydata,cdata,fill_value=-999):
-    ''' Remove pixels where x/y data have nodata, but 
-    fill pixels where cdata has nodata with a distinct value
-    '''
-
-    ###### remove nodata x/y variable
-    idx_nan = np.any( np.isnan( np.concatenate( [xdata, ydata] ,axis=1 )), axis=1) # (Nsamples, stack) --> (Nsamples, )
-    xdata = xdata[~idx_nan]
-    ydata = ydata[~idx_nan]
-    cdata = cdata[~idx_nan]
-
-    ### fill nodata of c-variable
-    idx_nan_cdata = np.isnan(cdata)
-    xdata[idx_nan_cdata] = fill_value
-    ydata[idx_nan_cdata] = fill_value
-    cdata[idx_nan_cdata] = fill_value
-
-    return xdata, ydata, cdata
 
 def reproject_match_grid( ref_img_da, img_da , resample_method=rio.enums.Resampling.nearest, nodata_value=np.nan):
     ''' Match xarray grid of different spatial resolutions. Input should be dataArray'''
@@ -228,42 +151,6 @@ def repeat_static_variable_timeseries( region_ds , varname_to_repeat ):
     return region_ds
 
 
-def clip_and_aggregate_to_df( region_ds, iceshelf_polygon_gpd):
-    ''' --------------
-    Aggregate values to 1D: ON xr.DATASET
-    - Temporaly static variables should already be repeated, to match datashape
-    - Clip 2D to ice shelf (TO DO: clip to annual ice shelf) (2D xarray)
-    - Reshape to 1D by xarray.stack (spatial information is retained)
-    - Remove all pixels that have NaN value in any one of the variables (TO DO: do this per year, to account for changing ice shelf polygon) 
-    - Format arrays to a pandas dataFrama
-    --------------'''
-
-    ''' Clip to iceshelf  '''
-    region_ds  = region_ds.rio.clip( iceshelf_polygon_gpd.geometry, iceshelf_polygon_gpd.crs, drop=False, invert=False)
-
-    ''' Aggregate to 1D '''
-    region_ds_1d = region_ds.stack(samples=['x','y']) # (time, samples)
-
-    ''' Convert to dataFrame '''
-    data_pxs_df = region_ds_1d.to_dataframe() # nested dataframe
-
-    # Flatten the nested multi-index to just column values -- automatically generates a 'year' value for every sample
-    data_pxs_df = data_pxs_df.reset_index(level=['time','x','y']) # 18767504 rows
-    # Drop spatial ref (has not data acutally) 
-    data_pxs_df = data_pxs_df.drop(['spatial_ref'],axis=1)
-    # For now: drop x and y values; For spatial k-fold: do not drop x and y
-    # data_pxs_df = data_pxs_df.drop(['x','y'],axis=1)
-
-    ''' Drop NaN pixels:
-    Pandas drops all rows that contain missing values. 
-    - This means that if any variable has a NaN value, that px is dropped.
-    --> should make sure to fill NaN values for variables before this step (e.g. dmg NaN is set to 0; filling of REMA gaps)
-    - Since I have rows for px per year, this means that if I would have clipped the data to annual ice shelf polygons, the number of pixels per year can vary.  
-    '''
-    data_pxs_df.dropna(axis='index',inplace=True) # Drop rows which contain missing values.
-    data_pxs_df.head()
-
-    return data_pxs_df
 
 
 def downsample_dataArray_withoutD0( region_ds , ksize=3, boundary_method='pad', downsample_func = 'mean', skipna=None,verbose=True):
@@ -365,43 +252,6 @@ def calculate_velo_strain_features(data_ds, velocity_names=('xvelsurf','yvelsurf
 
 
 
-def make_regular_grid_for_ds(ds, grid_res):
-    x_seq = np.arange(ds.x.min(), ds.x.max()+grid_res, step=grid_res )
-    y_seq = np.arange(ds.y.min(), ds.y.max()+grid_res, step=grid_res )
-
-    # dimension in dataset:
-    dims = ds.dims
-    if 'time' in dims:
-        t_seq = ds.time # .values
-        grid_dummy = xr.DataArray(
-            data=np.ones( (len(t_seq), len(y_seq),len(x_seq) ) ), # dummy values
-            dims=["time", "y", "x"],
-            coords=dict( # proper coordinate/time values
-                time=t_seq, 
-                y=y_seq,
-                x=x_seq,
-            ),
-            attrs=dict(
-                description="dummy_regular_grid",
-            ),
-        ).rio.write_crs(3031)
-
-    else:
-        grid_dummy = xr.DataArray(
-            data=np.ones( (len(y_seq), len(x_seq)) ),
-            dims=["y", "x"],
-            coords=dict(
-                y=y_seq,
-                x=x_seq,
-            ),
-            attrs=dict(
-                description="dummy_regular_grid",
-            ),
-        ).rio.write_crs(3031)
-
-
-    return grid_dummy
-
 
 def drop_spatial_ref(ds):
     try:
@@ -410,78 +260,6 @@ def drop_spatial_ref(ds):
         pass
     return ds 
 
-def load_nc_obs_data( path2data, region_ID, varname=None, parts=['part1'],verbose=True):
-    ''' ----------------------
-    Load data: netCDFs per region, per variable
-    ------------------------- '''
-    if verbose:
-        print('----\n Loading netCDF for region ', region_ID)
-
-    ## Retrieve list of files for single/multi variable
-    if not varname:
-        ''' Load all variables from individual netCDF files '''
-        filelist = glob.glob(path2data + '*region-'+region_ID+'_*.nc')
-        filelist = [file for file in filelist if 'all2' not in file]
-        filelist = [file for file in filelist if 'strain' not in file]
-        filelist.sort()              
-    else: 
-        ''' Load files of single variable'''
-        filelist = glob.glob(path2data + '*region-'+region_ID+ '_*'+ varname +'*nc')
-        filelist.sort()
-
-    ## Filter list for desired 'parts' 
-    for part in parts:
-        if not part in ['part1','part2','1997']:
-            raise ValueError('Selected partition {} not in [''part1'',''part2'',''1997'']'.format(part))
-
-    
-    # for part in parts:
-    #     files_include = [file for file in filelist if part in file]
-    #     # region_filelist += files_include
-
-    #     # region_ds = xr.open_mfdataset( region_filelist,  
-    #                 combine="by_coords",decode_times=True,
-    #                 data_vars='minimal', 
-    #                 coords= 'minimal', 
-    #                 compat='broadcast_equals', #  all values must be equal when variables are broadcast against each other to ensure common dimensions.
-    #             #   chunks={'y':'auto','x':'auto','time':5}, # add chucnking info for dask
-    #                 chunks={'y':2000,'x':2000,'time':5}, # add chucnking info for dask: multitude of downsampling size
-    #                 )  
-    #     print('Loaded variables: \n', list(region_ds.keys()) )
-    # else:
-
-    if len(parts) == 1:
-        part=parts[0]
-        ''' Load selected variable '''
-        # # region_varfile = glob.glob(path2data + '*region-'+region_ID+ '_*'+ varname +'_'+part+'*nc')[0]
-        region_varfile = [file for file in filelist if part in file]
-        if len(region_varfile)>1:
-            raise ValueError('Expected 1 file for part {}, found: {}'.format(part, region_varfile) )
-            
-        region_varfile = region_varfile[0]
-        # load single file
-        region_ds = xr.open_dataset( region_varfile )
-
-    else: # combine all parts
-        # filelist = glob.glob(path2data + '*region-'+region_ID+ '_*'+ varname +'*nc')
-        region_filelist = []
-        for part in parts:
-            files_include = [file for file in filelist if part in file]
-            region_filelist += files_include
-        
-        with xr.open_mfdataset( region_filelist,  
-                combine="by_coords",decode_times=True,
-                data_vars='minimal', 
-                coords= 'minimal', 
-                compat='broadcast_equals', #  all values must be equal when variables are broadcast against each other to ensure common dimensions.
-                chunks={'y':2000,'x':2000,'time':1}, # add chucnking info for dask: multitude of downsampling size
-                # engine='scipy',
-                )  as region_ds:
-                print('opening region_ds')
-
-    print('Loaded variables: \n', list(region_ds.keys()) )
-
-    return region_ds
 
 
 
